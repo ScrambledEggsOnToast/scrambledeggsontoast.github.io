b39ba780fe07443a372023be2b713ad37fa79389
joshkirklin <tekn04321@gmail.com>
2014-05-07 16:31:20 +0100

    Animated scrolling for toc

--- public/js/toc.js
+++ public/js/toc.js
@@ -1,6 +1,7 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
 (function($){
   $.fn.toc = function(options) {
+
     var defaults = {
       noBackToTopLinks: false,
       title: '<p class="toc-header">Contents</p>',
@@ -24,32 +25,27 @@
     var level = get_level(headers[0]),
       this_level,
       html = settings.title + " <"+settings.listType+">" + return_to_top;
-    headers.on('click', function() {
-      if (!settings.noBackToTopLinks) {
-        window.location.hash = this.id;
-      }
-    })
-    .addClass('clickable-header')
-    .each(function(_, header) {
+    headers.each(function(_, header) {
       this_level = get_level(header);
       if (!settings.noBackToTopLinks && this_level === highest_level) {
         $(header).addClass('top-level-header').after(return_to_top);
       }
       if (this_level === level) // same level as before; same indenting
-        html += "<li><a href='#" + header.id + "'>" + header.innerHTML + "</a>";
+        html += "<li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
       else if (this_level < level) // higher level than before; end parent ol
-        html += "</li></"+settings.listType+"></li><li><a href='#" + header.id + "'>" + header.innerHTML + "</a>";
+        html += "</li></"+settings.listType+"></li><li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
       else if (this_level > level) // lower level than before; expand the previous to contain a ol
-        html += "<"+settings.listType+"><li><a href='#" + header.id + "'>" + header.innerHTML + "</a>";
+        html += "<"+settings.listType+"><li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
       level = this_level; // update for the next one
     });
     html += "</"+settings.listType+">";
     if (!settings.noBackToTopLinks) {
       $(document).on('click', '.back-to-top', function() {
-        $(window).scrollTop(0);
+        $('html,body').animate({scrollTop: 0},'fast');
         window.location.hash = '';
       });
     }
+
     if (0 !== settings.showSpeed) {
       output.hide().html(html).show(settings.showSpeed);
     } else {
