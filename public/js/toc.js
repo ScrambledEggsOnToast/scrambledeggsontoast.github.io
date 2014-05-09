a9d95505790e211349e15d97a4c6251347c752e9
joshkirklin <tekn04321@gmail.com>
2014-05-09 22:08:20 +0100

    added toc highlighting

--- public/js/toc.js
+++ public/js/toc.js
@@ -1,4 +1,4 @@
-// https://github.com/ghiculescu/jekyll-table-of-contents
+// based on https://github.com/ghiculescu/jekyll-table-of-contents
 (function($){
   $.fn.toc = function(options) {
 
@@ -17,23 +17,27 @@
     if (!headers.length || headers.length < 3 || !output.length) {
       return;
     }
-    
+    var offsetArr = $.map(headers, function(el) { 
+        return $(el).offset().top;
+    });
+    headers.splice(0,1);
+
     var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); }
     var highest_level = headers.map(function(_, ele) { return get_level(ele); }).get().sort()[0];
     var return_to_top = '<a class="icon-arrow-up back-to-top">return to top</a>';
     
     var level = get_level(headers[0]),
       this_level,
-      html = settings.title + " <"+settings.listType+">" + '<li><a class="back-to-top">Top</a></li>';
+      html = settings.title + " <"+settings.listType+">" + '<li class="toc-link"><div><a class="back-to-top">Top</a></div></li>';
     headers.each(function(_, header) {
       this_level = get_level(header);
 
       if (this_level === level) // same level as before; same indenting
-        html += "<li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
+        html += "<li class='toc-link'><div><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a></div>";
       else if (this_level < level) // higher level than before; end parent ol
-        html += "</li></"+settings.listType+"></li><li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
+        html += "</li></"+settings.listType+"></li><li class='toc-link'><div><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a></div>";
       else if (this_level > level) // lower level than before; expand the previous to contain a ol
-        html += "<"+settings.listType+"><li><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a>";
+        html += "<"+settings.listType+"><li class='toc-link'><div><a onClick=\"scrollTo('" + header.id + "')\">" + header.innerHTML + "</a></div>";
       level = this_level; // update for the next one
     });
     html += "</"+settings.listType+">";
@@ -49,5 +53,32 @@
     } else {
       output.html(html);
     }
+
+
+
+    var goToLinks = $("li.toc-link > div")
+
+
+    var atHeader = function (y) {
+        var yd = y + 0.3 * $(window).height();
+        var pasts = $.map(offsetArr, function(offset) {
+            return (offset < yd ? 1 : 0);
+        });
+        number = 0;
+        for (i = 0; i < pasts.length; i++) {
+            number += pasts[i];
+        }
+        return number-1;
+    }
+    
+    var updateLinkClasses = function() {
+        for (i = 0; i < goToLinks.length; i++) {
+            $(goToLinks[i]).removeClass("toc-current-section");
+        }
+        $(goToLinks[atHeader($(window).scrollTop())]).addClass("toc-current-section");
+    }
+
+    $(window).on('load', updateLinkClasses);
+    $(window).on('scroll', updateLinkClasses);
   };
 })(jQuery);
